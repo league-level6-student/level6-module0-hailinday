d5206017c93a3666145556f33859d76c932a9d25
league <league@imac-34.attlocal.net>
2023-12-21 18:27:28 -0800

    API new stuff

--- src/main/java/_02_cat_facts_API/CatFactsApi.java
+++ src/main/java/_02_cat_facts_API/CatFactsApi.java
@@ -4,6 +4,8 @@ import _02_cat_facts_API.data_transfer_objects.CatWrapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import _01_intro_to_APIs.data_transfer_objects.Result;
 import reactor.core.publisher.Mono;
 
 /*
@@ -34,16 +36,19 @@ public class CatFactsApi {
         Use the WebClient to make the request, converting the response to String.class.
         This request doesn't require url parameters, so you can omit the .uri() method call entirely
         */
-
+    	Mono<String> stringMono = webClient
+        		.get()
+                .retrieve()
+                .bodyToMono(String.class);
 
         //Collect the response from the Mono object
-
+    	String response = stringMono.block();
 
         /*
         Print out the actual JSON response -
         this is what you will input into jsonschema2pojo.com
          */
-
+        System.out.println(response);
 
         /*
         Use http://www.jsonschema2pojo.org/ to generate your POJO
@@ -56,24 +61,28 @@ public class CatFactsApi {
         */
     }
 
-    public String getCatFact() {
+    public CatWrapper getCatFact() {
 
         //Make the request, saving the response in an object of the type that you just created in your
         //data_transfer_objects package (CatWrapper)
-
+    	Mono<CatWrapper> catMono = webClient
+        		.get()
+                .retrieve()
+                .bodyToMono(CatWrapper.class);
         //Use block() to collect the response into a java object using the class you just created
-
+    	
         //return the Object
-        return null;
+        return catMono.block();
 
 
     }
 
     public String findCatFact(){
         //use the getCatFact method to retrieve a cat fact
-
+    	CatWrapper catWrap = getCatFact();
         //return the first (and only) String in the Arraylist of data in the response
-        return null;
+    	String catFact = catWrap.getData().get(0);
+        return catFact;
     }
 
     public void setWebClient(WebClient webClient) {
--- src/main/java/_02_cat_facts_API/CatFactsRunner.java
+++ src/main/java/_02_cat_facts_API/CatFactsRunner.java
@@ -1,12 +1,14 @@
 package _02_cat_facts_API;
 
+import _02_cat_facts_API.data_transfer_objects.CatWrapper;
+
 public class CatFactsRunner {
 
     public static void main(String[] args) {
         CatFactsApi catFactsApi = new CatFactsApi();
         catFactsApi.testRequest();
-//        String catFact = catFactsApi.getCatFact();
-//        System.out.println(catFact);
+        String catFindFact = catFactsApi.findCatFact();
+        System.out.println(catFindFact);
     }
 
 }
