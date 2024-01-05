93ece40e9d89a4ac2c93d7afabbf9246f6085094
league <league@imac-12.attlocal.net>
2024-01-04 18:32:30 -0800

    donuts

--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -9,26 +9,51 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import _08_mocking.models.DeliveryDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
+import java.util.List;
+
 class MyDonutShopTest {
 
     MyDonutShop myDonutShop;
-
+    
+    @Mock
+    DeliveryService deliveryService;
+    
+    @Mock
+    BakeryService bakeryService;
+    
+    @Mock
+    PaymentService paymentService;
+    
     @BeforeEach
     void setUp() {
-
+    	 MockitoAnnotations.openMocks(this);
+    	 
+         myDonutShop = new MyDonutShop(paymentService, deliveryService, bakeryService);
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
         //given
+    	Order order = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
+    	myDonutShop.openForTheDay();
+        when(bakeryService.getDonutsRemaining()).thenReturn(order.getNumberOfDonuts()+1);
 
         //when
-
+    	myDonutShop.takeOrder(order);
         //then
+    	verify(deliveryService, times(1)).scheduleDelivery(order);;
     }
 
     @Test
