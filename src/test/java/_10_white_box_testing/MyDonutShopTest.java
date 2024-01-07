6367593f5bea91adb83c9eea6a63eaf27c7be0a2
hailinday <54258211+hailinday@users.noreply.github.com>
2024-01-07 12:54:52 -0800

    White box testing

--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -13,6 +13,7 @@ import _08_mocking.models.DeliveryDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import java.util.Collections;
@@ -49,29 +50,56 @@ class MyDonutShopTest {
                 true);
     	myDonutShop.openForTheDay();
         when(bakeryService.getDonutsRemaining()).thenReturn(order.getNumberOfDonuts()+1);
-
+        when(paymentService.charge(order)).thenReturn(true);
         //when
     	myDonutShop.takeOrder(order);
         //then
-    	verify(deliveryService, times(1)).scheduleDelivery(order);;
+    	verify(deliveryService, times(1)).scheduleDelivery(order);
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
-
+    	Order order = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
+    	myDonutShop.openForTheDay();
+    	when(bakeryService.getDonutsRemaining()).thenReturn(order.getNumberOfDonuts()-1);
         //when
-
+    	Throwable exceptionThrown = assertThrows(Exception.class, () -> myDonutShop.takeOrder(order));
+        assertEquals(exceptionThrown.getMessage(), "Insufficient donuts remaining");
         //then
+        try {
+			verify(deliveryService, never()).scheduleDelivery(order);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
 
     @Test
     void givenNotOpenForBusiness_whenTakeOrder_thenThrowIllegalStateException(){
         //given
-
+    	Order order = new Order("CUSTOMER_NAME",
+                "CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
         //when
-
+    	Throwable exceptionThrown = assertThrows(Exception.class, () -> myDonutShop.takeOrder(order));
+        assertEquals(exceptionThrown.getMessage(), "Sorry we're currently closed");
         //then
+        try {
+			verify(deliveryService, never()).scheduleDelivery(order);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }
